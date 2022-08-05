d27d3f75f757b203fb4e8b32e9050e794fd1eb61
Viktor Edman <viktor@edman.se>
2022-08-05 14:00:27 +0200

    did some stuff

--- myApp.js
+++ myApp.js
@@ -1,10 +1,15 @@
 let express = require('express');
 let app = express();
+app.enable('trust proxy')
+let bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended: false}))
+
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.path} - ${req.ip}`);
     next()
 })
-app.enable('trust proxy')
+
 require('dotenv').config()
 console.log(process.env.MESSAGE_STYLE)
 
@@ -30,6 +35,22 @@ app.get('/now', (req, res, next) => {
     res.json({time: req.time})
 })
 
+app.get('/:word/echo', (req, res) => {
+    res.json({echo: req.params.word})
+})
+
+app
+    .route('/name')
+    .get((req,res) => {
+        let firstName = req.query.first
+        let lastName = req.query.last
+        res.json({name: `${firstName} ${lastName}`})
+    })
+    .post((req, res) =>{
+        let firstName = req.body.first
+        let lastName = req.body.last
+        res.json({name: `${firstName} ${lastName}`})
+    })
 
 
 
