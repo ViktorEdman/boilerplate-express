e4eccc2e44d521d284d424d28e8f67c54772c8ae
Viktor Edman <viktor@edman.se>
2022-08-05 13:11:17 +0200

    added my own fork

--- myApp.js
+++ myApp.js
@@ -1,7 +1,27 @@
 let express = require('express');
 let app = express();
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next()
+})
+app.enable('trust proxy')
+require('dotenv').config()
+console.log(process.env.MESSAGE_STYLE)
 
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + "/views/index.html")
+})
 
+app.use('/public', express.static(__dirname + "/public"))
+
+app.get('/json', (req,res) => {
+    let messageStyle = process.env.MESSAGE_STYLE;
+    let message = "Hello json";
+    if (messageStyle === "uppercase") {
+        message = message.toUpperCase()
+    }
+    res.json({"message": message})
+})
 
 
 
