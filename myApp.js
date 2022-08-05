69c32efa40525604423e9d8ae351c5e0f6cc5622
Viktor Edman <viktor@edman.se>
2022-08-05 13:16:15 +0200

    this is a commit

--- myApp.js
+++ myApp.js
@@ -23,6 +23,13 @@ app.get('/json', (req,res) => {
     res.json({"message": message})
 })
 
+app.get('/now', (req, res, next) => {
+    req.time = new Date().toString()
+    next();
+}, (req, res) => {
+    res.json({time: req.time})
+})
+
 
 
 
